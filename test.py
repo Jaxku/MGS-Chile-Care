475ae95a8d2ce84299802970d0353940e36ab04c
Jaxku <52820208+Jaxku@users.noreply.github.com>
2023-02-15 10:28:38 +1300

    e

---
+++ test.py
@@ -0,0 +1,18 @@
+choice=0
+
+
+while choice != 5:
+    print("___________________________________________")
+    print("Welcome to MGS Childcare")
+    print("What would you like do today here at MGS Childcare")
+    print("___________________________________________")
+    print()
+    print("1. Drop off a child")
+    print("2. Pick up a child")
+    print("3 Calculate cost")
+    print("4. Print roll")
+    print("5. Exit")
+
+
+
+
