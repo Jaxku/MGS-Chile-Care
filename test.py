258bea4fb84462d8ac009cf30d8d85730d3597b0
Jaxku <52820208+Jaxku@users.noreply.github.com>
2023-02-16 15:15:07 +1300

    wefwef

--- test.py
+++ test.py
@@ -1,37 +1,35 @@
 
 
 def menu():
-        choice = ""
-        while choice != "5":
-            print("___________________________________________")
-            print("Welcome to MGS Childcare")
-            print("What would you like do today here at MGS Childcare")
-            print("___________________________________________")
-            print()
-            print("1. Drop off a child")
-            print("2. Pick up a child")
-            print("3 Calculate cost")
-            print("4. Print roll")
-            print("5. Exit")
-            print()
-
+    choice = ""
+    while choice != "5":
+        print("___________________________________________")
+        print("Welcome to MGS Childcare")
+        print("What would you like do today here at MGS Childcare")
+        print("___________________________________________")
+        print()
+        print("1. Drop off a child")
+        print("2. Pick up a child")
+        print("3 Calculate cost")
+        print("4. Print roll")
+        print("5. Exit")
+        print()
 
         choice = int(input("Enter your choice (number between 1 and 5): "))
-        if choice==1:
+        if choice == "1":
                 dropOff()
 
-        elif choice==2:
+        elif choice == "2":
                 pickUp()
 
-        elif choice==3:
+        elif choice == "3":
                 calcCost()
 
-        elif choice==4:
+        elif choice == "4":
                 printRoll()
 
-        elif choice==5:
+        elif choice == "5":
              print("Goodbye")
-
 
         else:
                 print("Invaild Response, Please try again")
